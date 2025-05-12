7e30a44895405cf5eb6be9a20db8626afee34661
IkerLopezGomez <a24ikelopgom@inspedralbes.cat>
2025-05-12 13:53:02 +0200

    db modificada

--- db_init/create.sql
+++ db_init/create.sql
@@ -1,3 +1,42 @@
+-- Aquest script NOMÉS s'executa la primera vegada que es crea el contenidor.
+-- Si es vol recrear les taules de nou cal esborrar el contenidor, o bé les dades del contenidor
+-- és a dir, 
+-- esborrar el contingut de la carpeta db_data 
+-- o canviant el nom de la carpeta, però atenció a no pujar-la a git
+
+
+-- És un exemple d'script per crear una base de dades i una taula
+-- i afegir-hi dades inicials
+
+-- Si creem la BBDD aquí podem control·lar la codificació i el collation
+-- en canvi en el docker-compose no podem especificar el collation ni la codificació
+
+-- Per assegurar-nes de que la codificació dels caràcters d'aquest script és la correcta
+SET NAMES utf8mb4;
+
+CREATE DATABASE IF NOT EXISTS a24ikelopgom_Proyecto
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+-- Donem permisos a l'usuari 'usuari' per accedir a la base de dades 'a24ikelopgom_Proyecto'
+-- sinó, aquest usuari no podrà veure la base de dades i no podrà accedir a les taules
+GRANT ALL PRIVILEGES ON a24ikelopgom_Proyecto.* TO 'usuari'@'%';
+FLUSH PRIVILEGES;
+
+
+-- Després de crear la base de dades, cal seleccionar-la per treballar-hi
+USE a24ikelopgom_Proyecto;
+
+
+-- Base de dades: a24ikelopgom_Proyecto
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula Actuacions
+--
+
 DROP TABLE IF EXISTS `Actuacions`;
 CREATE TABLE `Actuacions` (
   `cod_incidencia` int NOT NULL,
