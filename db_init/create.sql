c6270a032ad751a14abaca63b4a5ffdabcac52f5
IkerLopezGomez <a24ikelopgom@inspedralbes.cat>
2025-05-12 13:09:29 +0200

    bd de datos modificdada

--- db_init/create.sql
+++ db_init/create.sql
@@ -37,167 +37,75 @@ USE a24ikelopgom_Proyecto;
 -- Estructura de la taula `Actuacions`
 --
 
+DROP TABLE IF EXISTS `Actuacions`;
 CREATE TABLE `Actuacions` (
-  `cod_incidencia` int(30) NOT NULL,
-  `cod_actuacio` int(255) NOT NULL,
-  `cod_tecnic` int(255) NOT NULL,
+  `cod_incidencia` int NOT NULL,
+  `cod_actuacio` int NOT NULL AUTO_INCREMENT,
+  `cod_tecnic` int NOT NULL,
   `data_actuacio` date NOT NULL,
-  `temps_dedicat` varchar(255) NOT NULL,
-  `descripcio` varchar(1000) NOT NULL
+  `temps_dedicat` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `descripcio` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`cod_actuacio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estructura de la taula `Departament`
---
-
+DROP TABLE IF EXISTS `Departament`;
 CREATE TABLE `Departament` (
-  `cod_depart` int(30) NOT NULL,
-  `nom_depart` varchar(255) NOT NULL,
-  `consum_depart` varchar(255) NOT NULL,
-  `temps_dedicat` varchar(255) NOT NULL
+  `cod_depart` int NOT NULL,
+  `nom_depart` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `consum_depart` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `temps_dedicat` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`cod_depart`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `Departament` (`cod_depart`, `nom_depart`, `consum_depart`, `temps_dedicat`) VALUES
+(1,	'RRHH',	'0',	'0'),
+(2,	'Contabilidad',	'0',	'0'),
+(3,	'Marketing',	'0',	'0'),
+(4,	'Direccion',	'0',	'0'),
+(5,	'Informatica',	'0',	'0');
 
-
-
-INSERT INTO Departament (cod_depart, nom_depart, consum_depart, temps_dedicat) VALUES (1, "RRHH", 0, 0);
-INSERT INTO Departament (cod_depart, nom_depart, consum_depart, temps_dedicat) VALUES (2, "Contabilidad", 0, 0);
-INSERT INTO Departament (cod_depart, nom_depart, consum_depart, temps_dedicat) VALUES (3, "Marketing", 0, 0);
-INSERT INTO Departament (cod_depart, nom_depart, consum_depart, temps_dedicat) VALUES (4, "Direccion", 0, 0);
-INSERT INTO Departament (cod_depart, nom_depart, consum_depart, temps_dedicat) VALUES (5, "Informatica", 0, 0);
-
-
-
-
-
-
-
-
--- --------------------------------------------------------
-
---
--- Estructura de la taula `Incidencies`
---
-
+DROP TABLE IF EXISTS `Incidencies`;
 CREATE TABLE `Incidencies` (
-  `cod_incidencia` int(30) NOT NULL,
-  `cod_tecnic` int(255) DEFAULT NULL,
-  `departament` varchar(255) NOT NULL,
-  `estat` varchar(255) NOT NULL DEFAULT 'Oberta',
+  `cod_incidencia` int NOT NULL AUTO_INCREMENT,
+  `cod_tecnic` int DEFAULT NULL,
+  `departament` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `estat` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Oberta',
   `data` datetime NOT NULL,
-  `descripcio` text NOT NULL
+  `descripcio` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`cod_incidencia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estructura de la taula `Informes`
---
-
+DROP TABLE IF EXISTS `Informes`;
 CREATE TABLE `Informes` (
-  `cod_incidencia` int(30) NOT NULL,
-  `cod_tecnic` int(255) NOT NULL,
+  `cod_incidencia` int NOT NULL AUTO_INCREMENT,
+  `cod_tecnic` int NOT NULL,
   `data_incidencia` date NOT NULL,
-  `temps_dedicat` varchar(255) NOT NULL
+  `temps_dedicat` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`cod_incidencia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estructura de la taula `Tecnics`
---
-
+DROP TABLE IF EXISTS `Tecnics`;
 CREATE TABLE `Tecnics` (
-  `cod_tecnic` int(30) NOT NULL,
-  `total_incidencies` varchar(255) NOT NULL
+  `cod_tecnic` int NOT NULL,
+  `nom_tecnic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`cod_tecnic`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `Tecnics` (`cod_tecnic`, `nom_tecnic`) VALUES
+(1,	'Jesus Rodriguez'),
+(2,	'Francisco Alarcon'),
+(3,	'Aitor Ruibal'),
+(4,	'Ohian Sancet'),
+(5,	'Lamine Yamal');
 
-
-INSERT INTO Tecnics (cod_tecnic, total_incidencies) VALUES (1, 0);
-INSERT INTO Tecnics (cod_tecnic, total_incidencies) VALUES (2, 0);
-INSERT INTO Tecnics (cod_tecnic, total_incidencies) VALUES (3, 0);
-INSERT INTO Tecnics (cod_tecnic, total_incidencies) VALUES (4, 0);
-INSERT INTO Tecnics (cod_tecnic, total_incidencies) VALUES (5, 0);
-
-
--- --------------------------------------------------------
-
---
--- Estructura de la taula `Usuaris`
---
-
+DROP TABLE IF EXISTS `Usuaris`;
 CREATE TABLE `Usuaris` (
-  `id` int(30) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `departament` varchar(255) NOT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `departament` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índexs per a les taules bolcades
---
-
---
--- Índexs per a la taula `Actuacions`
---
-ALTER TABLE `Actuacions`
-  ADD PRIMARY KEY (`cod_actuacio`);
-
---
--- Índexs per a la taula `Departament`
---
-ALTER TABLE `Departament`
-  ADD PRIMARY KEY (`cod_depart`);
-
---
--- Índexs per a la taula `Incidencies`
---
-ALTER TABLE `Incidencies`
-  ADD PRIMARY KEY (`cod_incidencia`);
-
---
--- Índexs per a la taula `Informes`
---
-ALTER TABLE `Informes`
-  ADD PRIMARY KEY (`cod_incidencia`);
-
---
--- Índexs per a la taula `Tecnics`
---
-ALTER TABLE `Tecnics`
-  ADD PRIMARY KEY (`cod_tecnic`);
-
---
--- Índexs per a la taula `Usuaris`
---
-ALTER TABLE `Usuaris`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT per les taules bolcades
---
-
-ALTER TABLE `Actuacions`
-  MODIFY `cod_actuacio` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la taula `Incidencies`
---
-ALTER TABLE `Incidencies`
-  MODIFY `cod_incidencia` int(30) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la taula `Informes`
---
-ALTER TABLE `Informes`
-  MODIFY `cod_incidencia` int(30) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la taula `Usuaris`
---
-ALTER TABLE `Usuaris`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-COMMIT;
